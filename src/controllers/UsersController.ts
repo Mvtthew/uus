40b7f7e8dd59f23e3291ad8441d6db2f5271e380
Mateusz Ożóg <mateusz2345678@gmail.com>
2020-04-12 12:50:19 +0200

    TS :<

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,13 +1,15 @@
-import express from 'express';
+import express, { Request, Response, response } from 'express';
+import { Observable } from 'rxjs';
 import Controller from "../types/Controller";
+import User from '../models/User';
 
 export default class UsersController extends Controller {
 
 	constructor() {
 		super();
 
-		this.router.get('/', (req, res) => {
-			res.json('asd');
+		this.router.post('/users', (req, res) => {
+			console.log(User.find().then(console.log));
 		});
 	}
 
