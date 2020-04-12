cbeaae54990c0682d0f924dde476151a347418e1
Mateusz Ożóg <mateusz2345678@gmail.com>
2020-04-12 14:01:03 +0200

    :whale: Docker++

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('./database/connect');
 // Initialize routes
 app.use(require('./routes'));
 
-const PORT = process.env.PORT || 21010;
+const PORT = process.env.PORT || 7070;
 app.listen(PORT, () => {
 	console.log(`Server started on port //:${PORT}`);
 });
--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const { mongoUrl } = require('../config/config');
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }, (err) => {
+let mongoUrlFinal = mongoUrl;
+
+if (process.env.RUNDOCKER) {
+	mongoUrlFinal = 'mongodb://mongo:27017/uus';
+}
+
+mongoose.connect(mongoUrlFinal, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }, (err) => {
 	if (err) throw err;
 	console.log('Sucessfully connected to MongoDB database!');
 });
